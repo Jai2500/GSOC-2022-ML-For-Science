8485f69f9223d2fa8cbff68830916b006edcb12d
Jai2500 <jai.bardhan90@gmail.com>
2022-04-04 02:17:39 +0530

    Basic working Parquet reader

--- Task2/eda.py
+++ Task2/eda.py
@@ -1,9 +1,10 @@
 import pyarrow.parquet as pq
+import numpy as np
 
-run_0_path = "/scratch/gsoc/QCDToGGQQ_IMGjet_RH1all_jet0_run0_n36272.test.snappy.parquet"
-run_1_path = "/scratch/gsoc/QCDToGGQQ_IMGjet_RH1all_jet0_run1_n47540.test.snappy.parquet"
-run_2_path = "/scratch/gsoc/QCDToGGQQ_IMGjet_RH1all_jet0_run2_n55494.test.snappy.parquet"
+run_0_path = "/scratch/gsoc/parquet_ds/QCDToGGQQ_IMGjet_RH1all_jet0_run0_n36272.test.snappy.parquet"
+run_1_path = "/scratch/gsoc/parquet_ds/QCDToGGQQ_IMGjet_RH1all_jet0_run1_n47540.test.snappy.parquet"
+run_2_path = "/scratch/gsoc/parquet_ds/QCDToGGQQ_IMGjet_RH1all_jet0_run2_n55494.test.snappy.parquet"
 
-df = pq.read_table(run_1_path)
-
-print(df)
+df = pq.ParquetFile(run_1_path)
+value = df.read_row_group(0)['X_jets']
+print(np.array(value.to_pylist()[0]).shape)
