05fd62a4637aae2c611cff6ba7417f2af4fb1f36
Jai2500 <jai.bardhan90@gmail.com>
2022-04-14 15:31:27 +0530

    init for Task 3

---
+++ Task3/dataset.py
@@ -0,0 +1,27 @@
+import pyarrow.parquet as pq
+import numpy as np
+import torch 
+
+class ImageDatasetFromParquet(torch.utils.data.Dataset):
+    def __init__(self, filename, transforms=[], return_regress=False) -> None:
+        super().__init__()
+
+        self.file = pq.ParquetFile(filename)
+
+        self.return_regress = return_regress
+    
+    def __getitem__(self, idx, ):
+        row = self.file.read_row_group(idx).to_pydict()
+        to_return = {
+            "X_jets": self.transforms(np.array(row['X_jets'][0]).reshape(125, 125, 3)),
+            "y": row['y'][0]
+        }
+        
+        if self.return_regress:
+            to_return['pt'] = row['pt'][0]
+            to_return['m0'] = row['m0'][0]
+
+        return to_return
+
+    def __len__(self):
+        return self.file.num_row_groups
